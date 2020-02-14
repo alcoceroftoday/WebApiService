a984b8a9819204290ad38d2096fb77be62c544b6
David Alcocer <davidbryan.alcocer@processconsulting.net>
2020-02-14 18:05:43 -0500

    se realizo una consulta a mysql

--- src/main/java/io/vertx/WebApiService/Main.java
+++ src/main/java/io/vertx/WebApiService/Main.java
@@ -1,4 +1,31 @@
 package io.vertx.WebApiService;
 
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+
 public class Main {
+    public static void main(String[] args) {
+        MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+                .setPort(3306)
+                .setHost("localhost")
+                .setDatabase("prueba")
+                .setUser("david")
+                .setPassword("david");
+        PoolOptions poolOptions=new PoolOptions()
+                .setMaxSize(5);
+
+        MySQLPool client = MySQLPool.pool(connectOptions,poolOptions);
+        client.query("SELECT * FROM alumnos WHERE edad='20'", ar -> {
+            if (ar.succeeded()) {
+                RowSet<Row> result = ar.result();
+                System.out.println("Got " + result.size() + " rows ");
+            } else {
+                System.out.println("Failure: " + ar.cause().getMessage());
+            }
+            client.close();
+    });
+}
 }
